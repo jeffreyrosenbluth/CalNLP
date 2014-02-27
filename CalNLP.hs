b23e7de374921d64c099101b6fb357aa0b587cf8
jeffrey rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-02-27 10:58:09 -0500

    Simplified ParsecT types

--- CalNLP.hs
+++ CalNLP.hs
@@ -3,15 +3,20 @@
 
 module Main where
 
-import        Control.Applicative   (Applicative(..), (<$>), (<*>))
-import        Control.Arrow         (first)
-import        Control.Monad
-import        Data.Char             (toUpper, toLower)
-import        Text.Parsec
+import           Control.Applicative   (Applicative(..), (<$>), (<*>), (<$))
+import           Control.Arrow         (first)
+import           Control.Monad
+import           Data.Char             (toUpper, toLower)
+import           Data.Time
+import           Text.Parsec
+import           Text.Parsec.String
 
--- For Testing --------
+-- For Testing ----------------------------------------------------------------
 import        System.IO
------------------------
+
+today :: Day
+today = fromGregorian 2014 2 27
+-------------------------------------------------------------------------------
 
 data Date =
   Date { month :: Int
@@ -19,62 +24,88 @@ data Date =
 
 -- Utility functions ----------------------------------------------------------
 
-delim :: Stream s m Char => ParsecT s u m ()
+delim :: Parser ()
 delim = (skipMany1 punc <* spaces) <|> eof
 
-punc :: Stream s m Char => ParsecT s u m Char
+punc :: Parser Char
 punc = char ',' <|> char '.' <|> space
 
 -- | Return the parse of the first substring that parses successfully and the
 --   input 'String' with the match removed.
-find :: Parsec String () b -> String -> (Either ParseError b, String)
+find :: Parser b -> String -> (Either ParseError b, String)
 find p s = find' p s ""
   where
     find' p s accum =
-      case parse p' "" s of
+      case parse ((,) <$> p <*> getInput) "" s of
+        -- XXX What to do here? it would be cumbersome to accumulate all of
+        -- the errors from each attempt, giving a potentially long and
+        -- confustng error. On the other hand just return the error from the
+        -- last attempt is useless.
         Left err ->
           case s of
             [] -> (Left err, s)
-            (x:xs) -> find' p xs (accum ++ [x])
-        Right (y, str) -> (Right y, accum ++ str)
-      where
-        p' = (,) <$> p <*> getInput
+            (x:xs) -> find' p xs (x : accum)
+        Right (y, str) -> (Right y, (reverse accum) ++ str)
+
+-- Given today's date, a month and a year return the first date in the future
+-- with same day and month if possible. Otherwise clip that day to the first
+-- valid day in that month.
+impliedYear :: Day -> Int -> Int -> Day
+impliedYear today m d =
+    case dateGuess of
+        Just d -> if diffDays d today >= 0
+                  then d
+                  else addGregorianYearsClip 1 d
+        Nothing -> impliedYear today m (d-1)
+  where
+    (yyyy, mm, dd) = toGregorian today
+    dateGuess = fromGregorianValid (yyyy) m d
+
+
+-- Date words -----------------------------------------------------------------
+
+months :: [String]
+months = [ "january", "february", "march", "april", "may"
+         , "june", "july", "august", "september", "october"
+         , "november", "december"]
+
+nums :: [String]
+nums = [ "one", "two", "three", "four", "five", "six", "seven"
+       , "eight", "nine", "ten", "eleven", "twelve"]
+
+numth :: [String]
+numth = [ "first", "second", "third", "fourth", "fifth"
+        , "sixth", "seventh", "eighth", "nineth", "tenth"]
+
+days :: [String]
+days = [ "mon", "monday", "tue", "tues", "tuesday", "wed", "wednesday"
+       , "thu", "thurs", "thursday", "fri", "friday", "sat", "saturday"
+       , "sun", "sunday"]
 
 -- Months ---------------------------------------------------------------------
 
--- Create a parser for the name of a month. Return the month number paired
--- with the string that matched.
-mkMonthA :: Monad m => String -> Int -> ParsecT String u m Int
-mkMonthA name r = const r <$> oneOf a <* string a23 <* ending
+-- Create a parser for the name of a month. Return the month number.
+mkMonthA :: String -> Int -> Parser Int
+mkMonthA name r = r <$ oneOf a <* string a23 <* (option "" $ string as)
   where
     ((a1, a23), as) = first (splitAt 1) $ splitAt 3 name
     a = [toUpper (head a1), toLower (head a1)]
-    ending = option "" $ string as
 
--- | Parser for the name of the any month. Return the month number paired
---   with the string that matched.
-monthsA :: Monad m => ParsecT String u m Int
+-- | Parser for the name of the any month. Return the month number.
+monthsA :: Parser Int--Monad m => ParsecT String u m Int
 monthsA = choice . map try $ zipWith mkMonthA months [1..12]
-  where
-    months = [ "January", "February", "March", "April", "May"
-             , "June", "July", "August", "September", "October"
-             , "November", "December"]
 
 -- Days of the Month ----------------------------------------------------------
 
--- | Parser for numeric day of month. Return the day as an 'Int' paired
---   with the 'String' that matched.
-dayOfMonthN :: Monad m => ParsecT String u m Int
+-- | Parser for numeric day of month. Return the day as an 'Int'.
+dayOfMonthN :: Parser Int
 dayOfMonthN = do
   s <- many1 digit
   let n = read s :: Int
-  -- Allow for things like 31st, 23rd, 15th, and 2nd.
   suffix elem n [1,21,31] "st"
   suffix elem n [2,22] "nd"
   suffix elem n [3,23] "rd"
   suffix notElem n [1,2,3,21,31,22,23] "th"
-  -- Valid days of the month must be between 1 and 31, if the day exceeds
-  -- the number of days in a particular month it will be handled separately.
   if n > 0 && n <= 31
     then return n
     else parserFail "Day of month out of bounds"
@@ -82,29 +113,26 @@ dayOfMonthN = do
     suffix f m ds suf = if m `f` ds then opt suf else return ()
     opt str = optional (string str)
 
-mkDayOfMonthA :: Monad m => String -> Int -> ParsecT String u m Int
-mkDayOfMonthA name r = const r <$> string name
+mkDayOfMonthA :: String -> Int -> Parser Int
+mkDayOfMonthA name r = r <$ string name
 
 -- | Parser of a text day of month. We only support words up to the tenth
 --   day.
-dayOfMonthA :: Monad m => ParsecT String u m Int
-dayOfMonthA = choice . map try $ zipWith mkDayOfMonthA ds [1..10]
-  where
-    ds = [ "first", "second", "third", "fourth", "fifth"
-         , "sixth", "seventh", "eighth", "nineth", "tenth"]
+dayOfMonthA :: Parser Int
+dayOfMonthA = choice . map try $ zipWith mkDayOfMonthA numth [1..10]
 
-dayOfMonth :: Monad m => ParsecT String u m Int
+dayOfMonth :: Parser Int
 dayOfMonth = dayOfMonthN <|> dayOfMonthA
 
 -- | Parse a date, i.e a month and day of the month. We do not need the year.
-parseDateA :: Monad m => ParsecT String u m Date
+parseDateA :: Parser Date
 parseDateA = Date <$> monthsA
                    <* delim
                   <*> dayOfMonth
                    <* delim
 
 -- | Date in numeric form, e.g. 02/26/14---------------------------------------
-parseDateN :: Monad m => ParsecT String u m Date
+parseDateN :: Parser Date
 parseDateN = do
   ns <- sepBy1 (many1 digit) (char '/')
   let dm = map read ns :: [Int]
@@ -114,17 +142,15 @@ parseDateN = do
                then return (Date m d)
                else parserFail "Month of day out of range"
 
-parseDate :: Monad m => ParsecT String u m Date
+parseDate :: Parser Date
 parseDate = parseDateA <|> parseDateN
 
 -------------------------------------------------------------------------------
 
 main = do
-    withFile "testdata.txt" ReadMode
-      ( \handle -> do
-          contents <- hGetContents handle
-          let testStrings = lines contents
-              results = map (find parseDate) testStrings
-              rs = zip [1..141] results
-          mapM (putStrLn . show) rs
-      )
+  withFile "testdata.txt" ReadMode $ \handle -> do
+    contents <- hGetContents handle
+    let testStrings = lines contents
+        results = map (find parseDate) testStrings
+        rs = zip [1..] results
+    mapM (putStrLn . show) rs
